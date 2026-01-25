ff6e6b35b1dd1b3c7e7a1a270815ee10f78514d5
jvermeer <jrvermeer.dev@gmail.com>
2026-01-24 21:36:57 -0500

    media metadata

--- Psalter2025/src/app/services/audio-service.service.ts
+++ Psalter2025/src/app/services/audio-service.service.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@angular/core';
+import { ApplicationRef, ChangeDetectorRef, Injectable } from '@angular/core';
 import { PsalterService } from './psalter-service';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+    providedIn: 'root',
+    
+})
 export class AudioService {
 
-    constructor(private service: PsalterService) {
+    constructor(private service: PsalterService,
+        private cdRef: ApplicationRef) {
 
+        navigator.mediaSession?.setActionHandler('play', () => { this.toggleAudio(); this.cdRef.tick() })
+        navigator.mediaSession?.setActionHandler('pause', () => { this.toggleAudio(); this.cdRef.tick() })
+        navigator.mediaSession?.setActionHandler('stop', () => { this.cancelAudio(); this.cdRef.tick() })
     }
 
     audio: HTMLAudioElement;
@@ -30,6 +37,7 @@ export class AudioService {
             this.audio = new Audio(this.service.currentPsalter.audioFile);
             this.currentVerse = 1;
             this.audio.play();
+            this.updateMetadata();
             this.audio.onended = () => {
                 this.currentVerse++;
                 if (this.currentVerse > this.service.currentPsalter.verses.length)
@@ -38,6 +46,7 @@ export class AudioService {
                     this.isBetweenVerses = setTimeout(() => {
                         this.isBetweenVerses = null;
                         this.audio?.play()
+                        this.updateMetadata();
                     }, 700)
 
                 }
@@ -45,6 +54,25 @@ export class AudioService {
             this.service.currentPsalter$.subscribe(x => this.cancelAudio())
         }
     }
+
+    private updateMetadata() {
+        if (!navigator.mediaSession)
+            return;
+
+        navigator.mediaSession.metadata = new MediaMetadata({
+            title: `${this.service.currentPsalter.title} (${this.isPlaying})`,
+            //album: this.service.currentPsalter.identifier,
+            artist: `${this.service.currentPsalter.identifier} (${this.isPlaying})`,
+            artwork: [
+                {
+                    src: 'icons/maskable_icon_x512.png',
+                    sizes: '512x512',
+                    type: 'image/png'
+                }
+            ]
+        });
+    }
+
 
     public cancelAudio() {
         this.audio?.pause();
